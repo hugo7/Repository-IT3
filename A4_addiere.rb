061a6f3672945fb0076d3e247cea7dd66adbd033
hugo7 <dennis.hoffmann93@hotmail.de>
2016-02-02 19:02:35 +0100

    das ist ein Test

---
+++ A4_addiere.rb
@@ -0,0 +1,24 @@
+#Übungsblatt 4, Aufgabe 4
+
+#Funktionsdefintion
+def summe(x, y)
+	ergebnis = x + y
+	return ergebnis
+end
+
+#Zuweisung der Programmargumente
+a,b=ARGV
+
+#Ausgabe
+puts "Bitte zwei Zahlen eingeben"
+
+#Benutzereingabe
+a=gets.to_i
+b=gets.to_i
+
+
+#Aufruf der Funktion Summe
+wert=summe(a, b)
+
+#Bildschirmausgabe
+puts "#{a}+#{b}=#{wert}"
